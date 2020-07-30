7b07db4ec0a2b32292eb41d4a4b8c1f7f0a677fc
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-30 16:34:39 -0400

    [@Mavro] Add file upload.

--- lab10/app.ts
+++ lab10/app.ts
@@ -1,13 +1,10 @@
-/* import { Application, Router, Context } from "https://deno.land/x/oak/mod.ts";
-import { viewEngine, engineFactory, adapterFactory } from "https://deno.land/x/view_engine/mod.ts";
-import { upload } from "https://deno.land/x/upload_middleware_for_oak_framework/mod.ts";
- */
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import {
   viewEngine,
   engineFactory,
   adapterFactory,
 } from "https://deno.land/x/view_engine/mod.ts";
+import { upload } from "https://deno.land/x/upload_middleware_for_oak_framework/mod.ts";
 
 const ejsEngine = engineFactory.getEjsEngine();
 const oakAdapter = adapterFactory.getOakAdapter();
@@ -24,9 +21,18 @@ const indexController = ( ctx: any ) => {
 };
 
 const indexRouter: Router = new Router();
-indexRouter.get( '/', indexController );
+indexRouter
+    .get( '/', indexController )
+    .post('/upload', upload('uploads'), async(context: any, next: any) => {
+        const file = context.uploadedFiles;
+        console.log(file);
+        context.response.redirect('/');
+    })
 
 app.use( indexRouter.routes() );
+app.use(indexRouter.allowedMethods());
+
+console.log('App is litening on PORT 8000');
 
 await app.listen({ port: 8000 });
 
