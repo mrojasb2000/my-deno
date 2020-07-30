70625b560a028414285ec81adaf3bce4f7ea6b0f
Mauricio Rojas <mrojasb2000@gmail.com>
2020-07-30 17:17:34 -0400

    [@Mavro] Add style CSS and static files.

--- lab10/app.ts
+++ lab10/app.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import {
   viewEngine,
   engineFactory,
@@ -6,21 +6,33 @@ import {
 } from "https://deno.land/x/view_engine/mod.ts";
 import { upload } from "https://deno.land/x/upload_middleware_for_oak_framework/mod.ts";
 
+// Setting up boilerplate for view-engine
 const ejsEngine = engineFactory.getEjsEngine();
 const oakAdapter = adapterFactory.getOakAdapter();
 
+// Initiate app
+const indexRouter: Router = new Router();
 const app: Application = new Application();
 
+// Passing view-engine as middleware
 app.use( viewEngine( oakAdapter, ejsEngine, { 
   viewRoot: '.',
   viewExt: '.ejs'
 }));
 
+// Allowing Static file to fetch from server
+app.use(async (ctx,next) => {
+    await send(ctx, ctx.request.url.pathname,{
+     root: `${Deno.cwd()}/static`
+      })
+    next()
+   });
+
 const indexController = ( ctx: any ) => {
   ctx.render("index", { data: { name: "John" } });
 };
 
-const indexRouter: Router = new Router();
+// Creating Routes
 indexRouter
     .get( '/', indexController )
     .post('/upload', upload('uploads'), async(context: any, next: any) => {
@@ -29,11 +41,12 @@ indexRouter
         context.response.redirect('/');
     })
 
+// Adding middleware to require our router
 app.use( indexRouter.routes() );
 app.use(indexRouter.allowedMethods());
 
+// Making app to listen to port
 console.log('App is litening on PORT 8000');
-
 await app.listen({ port: 8000 });
 
 
